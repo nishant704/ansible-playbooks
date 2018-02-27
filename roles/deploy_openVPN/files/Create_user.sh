0f35b6c2dd8d53115e80991ff1a5ddd6524c4ba2
Nishant Singh <nishant2.singh@paytm.com>
2018-02-27 11:05:51 +0530

    OpenVPN working

--- roles/deploy_openVPN/files/Create_user.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-source vars
-./build-key "{{ username }}"
-google-authenticator --time-based --disallow-reuse --force --rate-limit=3 --rate-time=30 --window-size=3 -l "${KEY_NAME}" -s /etc/openvpn/otp/"{{ username }}".google_authenticator
-
-./build-key "{{ username }}"
